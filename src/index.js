2404462128a050e93eed1e921e2bdb5c7d73f2a1
Anzhelika Kalenik <anzhalikaremez@mail.ru>
2023-08-24 01:43:01 +0300

    feat: add function revers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+// module.exports = function reverse (n) {
+//     return Math.abs(n).toString().split('').reverse().join('');
+// }
+
 module.exports = function reverse (n) {
-  
+    var a=Math.abs(n);
+    var b=a.toString();
+    var c=b.split('');
+    var d=c.reverse();
+    var f=d.join('');
+
+    return f;
 }
